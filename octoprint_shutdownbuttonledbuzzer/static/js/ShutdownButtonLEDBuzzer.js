b5b985589deac6680a114d23e6f1b0df71fd1f54
brg995 <daniele.borgo@mail.polimi.it>
2021-12-15 23:59:26 +0100

    Implemented last updated for refresh

--- octoprint_shutdownbuttonledbuzzer/static/js/ShutdownButtonLEDBuzzer.js
+++ octoprint_shutdownbuttonledbuzzer/static/js/ShutdownButtonLEDBuzzer.js
@@ -9,22 +9,28 @@ $(function() {
     function ShutdownbuttonledbuzzerViewModel(parameters) {
         let self = this;
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
         self.settingsViewModel = parameters[0];
 
-        // TODO: Implement your plugin's view model here.
+        self.i2c_status = ko.observable("_")
+        self.spi_status = ko.observable("-")
+        self.updated_hour = ko.observable("-")
 
-        self.i2c_status = ko.observable("loading...")
-        self.spi_status = ko.observable("loading...")
+        self.set_hour = function (){
+            let date = new Date($.now())
+            self.updated_hour(date.toLocaleDateString() + " " + date.toLocaleTimeString())
+        }
 
         self.refresh = function (){
+            self.updated_hour("Updating...")
+            self.i2c_status("Loading...")
+            self.spi_status("Loading...")
             $.get(
                 API_BASEURL + "plugin/shutdownbuttonledbuzzer",
                 function (data) {
                     let parsed = JSON.parse(data)
                     self.i2c_status(parsed.i2c_status)
                     self.spi_status(parsed.spi_status)
+                    self.set_hour()
                 }
             )
         }
@@ -34,15 +40,9 @@ $(function() {
         }
     }
 
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
     OCTOPRINT_VIEWMODELS.push({
         construct: ShutdownbuttonledbuzzerViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ "settingsViewModel"],
-        // Elements to bind to, e.g. #settings_plugin_ShutdownButtonLEDBuzzer, #tab_plugin_ShutdownButtonLEDBuzzer, ...
-        elements: [ "#settings_plugin_shutdownbuttonledbuzzer"  ]
+        dependencies: [ "settingsViewModel" ],
+        elements: [ "#settings_plugin_shutdownbuttonledbuzzer" ]
     });
 });
